1e2fa72d95b064f9e75fab2dd6200a379e86f9e4
Aissa-Gue <AissaStarDz@gmail.com>
2021-05-08 00:38:46 +0100

    change index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,6 +12,6 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function showAllData(){
-        return view('welcome');
+        return view('index');
     }
 }
